50f637ccca260a6e6904da3f6f04bae009c9daa7
James Hatt <jhattenhauer@outlook.com>
2026-06-05 17:31:48 -0230

    basic arduino sketch

---
+++ arduino/read/read.ino
@@ -0,0 +1,20 @@
+const int analogPin = A0;
+int rawValue = 0;
+float voltage = 0.0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {q
+  rawValue = analogRead(analogPin);
+  voltage = rawValue * (5.0 / 1023.0);
+
+  Serial.print("Raw: ");
+  Serial.print(rawValue);
+  Serial.print("  |  Voltage: ");
+  Serial.print(voltage, 2);
+  Serial.println(" V");
+
+  delay(500);
+}
